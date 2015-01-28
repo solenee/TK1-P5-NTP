57bebad9681b90d734c9492c4f99f613b426e2f5
smndf <simon.dif.sd@gmail.com>
2015-01-28 14:24:26 +0100

    doesn't work

--- TK1-P5-NTP/src/ntp/timeclient/TimeClient.java
+++ TK1-P5-NTP/src/ntp/timeclient/TimeClient.java
@@ -3,23 +3,51 @@ package ntp.timeclient;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.Socket;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Random;
 
 public class TimeClient {
 	public final static int port = 2048;
 
-    public static void main (String[] str) {
-        // str [0] = nom machine
-        try {
-			
-            Socket s = new Socket("localhost",port);
-            BufferedReader d = new BufferedReader(new InputStreamReader(s.getInputStream()));
-            System.out.println("Answer from "+s.getRemoteSocketAddress()+":");
-            System.out.println(d.readLine());
-            s.close();
-        }
-        catch (Exception e) {
-            e.printStackTrace();
-        }
+	public static void main (String[] str) {
+		System.out.println("new client");
+		Client c = new Client(port);
+		c.start();
+	}
+}	
 
-    }
+class Client extends Thread {
+	//str [0] = nom machine
+
+	public static int port;
+	Date dateRequest;
+	public Client(int port){
+		this.port = port;
+	}
+
+	public void run() {
+		System.out.println("run client");
+		try {
+			dateRequest = new Date();
+			Random r = new Random();
+			sleep(r.nextInt(91) + 10); //random delay between 10 ms and 100 ms
+
+			Socket s = new Socket("localhost",port);
+			BufferedReader d = new BufferedReader(new InputStreamReader(s.getInputStream()));
+			System.out.println("Answer from "+s.getRemoteSocketAddress()+":");
+			String dateString = d.readLine();
+			System.out.println(dateString);
+			SimpleDateFormat dateParser=new SimpleDateFormat("EEE MMM dd HH:mm:ss zzz yyyy");
+			Date dateResponse = dateParser.parse(dateString);
+			double duration = dateResponse.getTime() - dateRequest.getTime();
+			System.out.println("duration = " + duration);
+			
+			s.close();
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
 }
--- TK1-P5-NTP/src/ntp/timeserver/TimeServer.java
+++ TK1-P5-NTP/src/ntp/timeserver/TimeServer.java
@@ -5,45 +5,49 @@ import java.io.PrintStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Date;
+import java.util.Random;
 
 public class TimeServer {
 
-	 public final static int port = 2048;
+	public final static int port = 2048;
 
-	    public static void main(String[] args) {
+	public static void main(String[] args) {
 
-	        ServerSocket ss;
+		ServerSocket ss;
 
-	        try {
-	            ss = new ServerSocket(port);
-	            while (true) {
-	                SlaveServer slv = new SlaveServer(ss.accept());
-	                slv.start();
-	            }	
-	        } catch (IOException e) {
-	            System.err.println(e);
-	        }
-	    } 
+		try {
+			ss = new ServerSocket(port);
+			while (true) {
+				SlaveServer slv = new SlaveServer(ss.accept());
+				slv.start();
+			}	
+		} catch (IOException e) {
+			System.err.println(e);
+		}
+	} 
+}
+
+
+class SlaveServer extends Thread {
+
+	Socket s;
+
+	public SlaveServer(Socket s) {
+		this.s = s;
 	}
 
-
-	class SlaveServer extends Thread {
-		
-	    Socket s;
-		
-	    public SlaveServer(Socket s) {
-	        this.s = s;
-	    }
-		
-	    public void run() {
-	        try {
-	            System.out.println (s.getRemoteSocketAddress() + " asks time.");
-	            PrintStream p = new PrintStream(s.getOutputStream());
-	            p.println(new Date());
-	            s.close();
-	        } catch (Exception e) {
-	            System.err.println(e);
-	        }
-	    }
+	public void run() {
+		try {
+			System.out.println (s.getRemoteSocketAddress() + " asks time.");
+			PrintStream p = new PrintStream(s.getOutputStream());
+			sleep(1000); // Artificial offset of 1000ms
+			Random r = new Random();
+	    	sleep(r.nextInt(91) + 10); //random delay between 10 ms and 100 ms
+			p.println(new Date());
+			s.close();
+		} catch (Exception e) {
+			System.err.println(e);
+		}
 	}
+}
 
